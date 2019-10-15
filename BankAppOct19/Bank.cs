34d66b954cef57e8b26fc8dc2a4e79f6fb30b1cd
Kal <info@kalacademy.org>
2019-10-15 12:09:01 -0700

    Initial commit

---
+++ BankAppOct19/Bank.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAppOct19
+{
+    class Bank
+    {
+        public Account CreateAccount(string accountName, string emailAddress, TypeOfAccount accountType, decimal initialDeposit)
+        {
+            var account = new Account
+            {
+                AccountName = accountName,
+                EmailAddress = emailAddress,
+                AccountType = accountType
+            };
+
+            if (initialDeposit > 0)
+            {
+                account.Deposit(initialDeposit);
+            }
+
+            return account;
+        }
+    }
+}
