2104069168510c0dce304319da889629a6cf91a3
Kal <info@kalacademy.org>
2019-10-15 12:14:01 -0700

    Added comment for git

--- BankAppOct19/Bank.cs
+++ BankAppOct19/Bank.cs
@@ -21,6 +21,8 @@ namespace BankAppOct19
             }
 
             return account;
+
+
         }
     }
 }
--- BankAppOct19/Program.cs
+++ BankAppOct19/Program.cs
@@ -6,6 +6,7 @@ namespace BankAppOct19
     {
         static void Main(string[] args)
         {
+            //Adding comment for git
             //object = instance of a class
             var account = new Account
             {
